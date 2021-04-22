62f0a3481721422c2d2ecb366a7c6b0d5e5949af
abdallahm15 <abdallah.magdy1515@gmail.com>
2021-04-22 14:37:57 +0200

    adding materialui table

--- src/Components/Courses/Courses-List.js
+++ src/Components/Courses/Courses-List.js
@@ -1,5 +1,13 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 class CoursesList extends React.Component {
     constructor(props) {
@@ -7,34 +15,50 @@ class CoursesList extends React.Component {
         this.delete = this.delete.bind(this)
     }
     render() {
+        const useStyles = makeStyles({
+            table: {
+                minWidth: 650,
+            },
+        });
+
+        const classes = useStyles();
+
         if (this.props.courses.length == 0)
             return (<div className="display-1 text-center text-primary">Loading ...</div>)
-        return (
-            <table className="table table-bordered">
-                <thead>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Grade</th>
-                    <th> </th>
-                </thead>
-                <tbody>
-                    {
-                        this.props.data.map(st => {
-                            return (<tr>
-                                <td>{st.id}</td>
-                                <td>{st.name}</td>
-                                <td>{st.grade}</td>
-                                <td>
+    
+            return (
+                <TableContainer component={Paper}>
+                    <Table className={classes.table} aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Id</TableCell>
+                                <TableCell align="right">Name</TableCell>
+                                <TableCell align="right">Duration</TableCell>
+                                <TableCell align="right">Topic</TableCell>
+                                <TableCell align="right">
                                     <a className="btn btn-info mr-1" onClick={this.select.bind(this, st)}>Edit</a>
                                     <a className="btn btn-danger" onClick={this.delete.bind(this, st.id)}>Delete</a>
-                                </td>
-                            </tr>);
-                        })
-                    }
-                </tbody>
-            </table>
+                                </TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {this.props.data.map((row) => (
+                                <TableRow key={row.Crs_Id}>
+                                    <TableCell component="th" scope="row">
+                                        {row.Crs_Id}
+                                    </TableCell>
+                                    <TableCell align="right">{row.Crs_Name}</TableCell>
+                                    <TableCell align="right">{row.Crs_Duration}</TableCell>
+                                    <TableCell align="right">{row.Topic.Top_Name}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            );
         )
     }
+
 
     delete(id) {
         var x = window.confirm("Are you sure?");
@@ -50,10 +74,3 @@ class CoursesList extends React.Component {
 }
 export default withRouter(CoursesList)
 
-//
-//
-//
-//
-//
-//
-//
